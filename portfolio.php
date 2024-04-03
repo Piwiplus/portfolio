4d7b7591dacd5a7cf369452d3e37ebb811e33926
piwiplus <matthieu.richard234@gmail.com>
2024-04-04 00:12:26 +0200

    first commit

---
+++ portfolio.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <meta charset="utf-8" />
+
+    <title>PORTFOLIO</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <header>
+      <nav class="nav-bar">
+        <ul>
+          <li class="merienda-uniquifier"><a href="#debut">Portfolio</a></li>
+          <li><a href="#projet">Projets</a></li>
+          <li><a href="#comp">Compétences</a></li>
+          <li><a href="#propos">à propos</a></li>
+        </ul>
+      </nav>
+    </header>
+    <div id="top-body" class="merienda-uniquifier">
+      <h1>By</h1>
+      <h1 id="name">Richard <br> Matthieu</h1>
+    </div>
+    <div class="anonymous-pro-regular">
+      <p>Développeur en  <br> Solution logicielle</p>
+      <div class="float">
+      <div class="carre" style="background-color:white;"></div>
+      <div class="carre" style="background-color:black;"></div>
+      </div>
+      </div>
+    <div>
+  </body>
+</html>
