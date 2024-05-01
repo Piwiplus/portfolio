d1936201c134ff3c715ae6c0f4f282debc22fc83
Matthieu <148204718+Piwiplus@users.noreply.github.com>
2024-05-01 22:04:01 +0200

    nouveau portfolio !

--- portfolio.php
+++ portfolio.php
@@ -3,45 +3,101 @@
   <meta charset="utf-8" />
   <title>PORTFOLIO</title>
   <link rel="stylesheet" href="style.css">
+  <link rel="icon" href="icon.png">
   <script src="fonction.js"></script> 
 </head>
 <body>
   <header id="header">
-    <h1 class="anonymous-pro-bold">Portfolio</h1>
+    <h1 class="anton-regular">Portfolio</h1>
     <nav>
       <ul>
-        <li class="anonymous-pro-bold"><a href="#projet">Projets</a></li>
-        <li class="anonymous-pro-bold"><a href="#">Compétences</a></li>
-        <li class="anonymous-pro-bold"><a href="#">À propos</a></li>
+        <li class="anton-regular"><a href="#projet">Projets</a></li>
+        <li class="anton-regular"><a href="#competence">Compétences</a></li>
+        <li class="anton-regular"><a href="#propos">À propos</a></li>
       </ul>
     </nav>
   </header>
-  
-  <div id="top-body" class="anonymous-pro-bold">
-    <p> 👋 Bienvenue dans mon portfolio ! Je suis Matthieu Richard, un étudiant passionné d'informatique</p>
+  <div id="top-body" class="anton-regular" >
+    <h4> 👋 Bienvenue dans mon portfolio ! Je suis Matthieu Richard, un étudiant passionné d'informatique.</p>
     
-    <div class="anonymous-pro-regular">
+    <div class="anton-regular">
       <div class="container">
         <div class="carre"> Développeur en Solution logicielle</div>
-        <div class="triangle"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud </div>
+        <div class="triangle"> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod</div>
       </div>
     </div>  
   </div>
-
-  <div id="projet"  id="lien-projet"  class="anonymous-pro-bold">
-    <h2>Mes Projets</h2>
+  <div id="projet"  id="lien-projet"  class="anton-regular">
+    <div class='top-contain'>
+        <h2 class='anton-regular'>Mes Projets</h2>
+        <hr></hr>
+    </div>
     <div class="container">
       <div class="carrePro">
         <h3>Projet 1</h3>
-        <p>Description du projet 1</p>
+        <img src="chemin/vers/votre/image.jpg" alt="Image de Compétence 2">
       </div>
       <div class="carrePro"> <!-- Utilisation de la classe carrePro pour le deuxième projet -->
-      <h3>Projet 2</h3>
-      <p>Description du projet 2</p>
-    </div>
-      <!-- Ajoutez plus de divs pour plus de projets -->
+      <img src="chemin/vers/votre/image.jpg" alt="Image de Compétence 2">
+        <p>Description du projet 2</p>
+      </div>
     </div>
   </div>
+  <div id="competence"  id="lien-competence"  class="anton-regular">
+    <div class='top-contain'>
+        <h2 class='anton-regular'>Mes Compétences & Environements</h2>
+        <hr></hr>
+    </div>
+    <div class="container">
+      <div class="carreComp">
+        <img class="Competence" src="php logo.png" alt="Image de Compétence 2">
+        <p>PHP</p>
+      </div>
+      <div class="carreComp"> <!-- Utilisation de la classe carrePro pour le deuxième projet -->
+      <img class="Competence" src="mysql logo.png" alt="Image de Compétence 2">
+        <p>MySQL</p>
+      </div>
+      <!-- Ajoutez plus de divs pour plus de projets -->
+      <div class="carreComp"> <!-- Utilisation de la classe carrePro pour le deuxième projet -->
+      <img class="Competence" src="html logo.png" alt="Image de Compétence 2">
+        <p>HTML</p>
+      </div>
+    </div>
 
+    <div class='top-contain'>
+    </div>
+    <div class="container">
+      <div class="carreComp">
+      <img class="Competence" src="css logo.png" alt="Image de Compétence 2">
+        <p>CSS</p>
+      </div>
+      <div class="carreComp"> <!-- Utilisation de la classe carrePro pour le deuxième projet -->
+      <img class="Competence" src="visual_studio.png" alt="Image de Compétence 2">
+        <p>Visual Studio</p>
+      </div>
+      <div class="carreComp"> <!-- Utilisation de la classe carrePro pour le deuxième projet -->
+      <img class="Competence" src="vscode_logo.png" alt="Image de Compétence 2">
+        <p>Visual Studio Code</p>
+      </div>
+  </div>
+  <div id="propos"  id="lien-propos"  class="anton-regular">
+    <div class='top-contain'>
+        <h2 class='anton-regular'>À Propos</h2>
+        <hr></hr>
+    </div>
+    <div class="container">
+      <div class="carreProp">
+      <img class="Propos"src="linkedin.webp" alt="Linkedin">
+        <p> Mon compte Linkedin</p>
+      </div>
+      <div class="carreProp"> <!-- Utilisation de la classe carrePro pour le deuxième projet -->
+      <img class="Propos" src="github.png" alt="Image de Compétence 2">
+        <p>Mon compte Github</p>
+      </div>
+    </div>
+  </div>
+  <footer>
+    <small>Copyright © 2024-2025  Tous droits réservés.</small>
+  </footer>
 </body>
 </html>
